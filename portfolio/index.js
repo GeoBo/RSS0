1691e39a4437f08d8374940bd5b908f82554a9bd
Geobo <bozhys@gmail.com>
2022-01-23 00:24:08 +0300

    feat: add gallery switcher

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,13 +1,53 @@
 window.addEventListener("load", function () {
+
+    addMenuEvent ();
+    addGalleryEvent ();
+    preloadImages ();
+});
+
+function preloadImages () {
+    const seasons = ['winter', 'spring', 'summer', 'autumn'];
+
+    seasons.forEach (season => {
+        for (let i = 1; i <= 6; i++) {
+            const img = new Image ();
+            img.src = `./assets/img/${season}/${i}.jpg`;
+        }
+    })     
+}
+
+function addGalleryEvent () {
+    const buttonBlock = document.querySelector(".button-block");
+    const btn = buttonBlock.querySelectorAll(".btn");
+    const portfolioImages = document.querySelectorAll(".portfolio-image");
+     
+    // console.log(btn); 
+    const toggleGallery = (event) => {
+        let activeBtn = event.target;
+        if (activeBtn.classList.contains("btn")) {
+            btn.forEach (button => {
+                button.classList.remove("active");
+            });
+            activeBtn.classList.add("active");
+            portfolioImages.forEach ((img, index) => 
+                img.src = `./assets/img/${activeBtn.dataset.season}/${index + 1}.jpg`
+            );
+        }
+    }
+
+    buttonBlock.addEventListener("click", toggleGallery);
+}
+
+function addMenuEvent () {
     const navList = document.querySelector(".nav-list");
-    const navLinks = document.querySelector(".nav-link");
     const overlay = document.querySelector(".overlay");
     const button = document.getElementById("menu-button");
 
     const toggleMenu = () => {
         if (navList.classList.contains("open")) {
             overlay.setAttribute("aria-hidden", "true");
-        } else overlay.setAttribute("aria-hidden", "");
+        } 
+        else overlay.setAttribute("aria-hidden", "");
 
         button.classList.toggle("active");
         navList.classList.toggle("open");
@@ -30,4 +70,4 @@ window.addEventListener("load", function () {
     overlay.addEventListener("click", closeMenu);
 
     //window.addEventListener("resize", closeMenu);
-});
+}
